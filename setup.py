97fa56d73855fe31ab6189b65bc40b6e9e79faba
Pavel <betadecay1993@gmail.com>
2024-09-27 14:48:42 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(name='trainRNNbrain',
       version='1.0',
       description='training rnns with various activation functions on user-defined tasks. '
                   'Anaylzing them with fixed and slow points analysis.',
-      url='https://github.com/engellab/trainRNNbrain/',
-      author='Pavel Tolmachev',
-      author_email='pt1290@princeton.edu',
+      url='***',
+      author='***',
+      author_email='***',
       license='MIT',
       packages=find_packages(),
       zip_safe=False)
